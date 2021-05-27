c420f63df2c6bc8de27e7d1d0279deb4f39bc66d
Savely Sakun <savely.sakun@gmail.com>
2021-05-27 21:46:02 +0500

    Initial commit

---
+++ MoodTracker/Diary/DiaryCell.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class DiaryCell: UITableViewCell {
+
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		setLayout()
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	private func setLayout() {
+
+	}
+
+}
