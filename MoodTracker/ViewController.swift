63e0c88a04a9ac983f80fad4f5e1933479f80e93
Savely Sakun <savely.sakun@gmail.com>
2021-05-27 21:31:59 +0500

    Initial Commit

---
+++ MoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by Savely Sakun on 27.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
